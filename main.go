1c22620ce2cd4d1b662a32eb6afd492f02579326
Bemax Ng <bemax@ngpro.local>
2025-02-08 22:04:36 +0100

    add healthz path and handler

--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
