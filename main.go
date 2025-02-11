ef3b2d6c6d8b35bce7ec7eb39e72e577628c74d9
Bemax Ng <bemax@ngpro.local>
2025-02-11 22:57:19 +0100

    Add metrics middleware

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprint("Hits: ", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetHandler(_ http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func healthzHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -12,9 +35,14 @@ func healthzHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	cfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetrics(http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+	mux.HandleFunc("/reset", cfg.resetHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
